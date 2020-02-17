b34522bede04f21ae5c56e725c45d028f6e47db4
Patrick Heron <hpa434@gmail.com>
2020-02-17 16:50:00 -0500

    cant randomize the next set

---
+++ javascript/java.js
@@ -0,0 +1,33 @@
+
+let numbers=[]
+let div = $("#numbers")
+
+let number = (min,max) => {
+   return Math.floor(Math.random() * (max - min + 1) ) + min;
+   }
+
+
+let generator = () => {
+
+   for (let i = 1; i <= 9; i++) {
+      numbers.push(number(1,10))
+      }
+   console.log(numbers)
+
+   div.prepend(`<p class=chosen>`)
+   $(".chosen").text(numbers)
+
+   if (numbers.length === 8) {
+      numbers=[]
+      }
+   }
+   for (let i = 1; i <= 10; i++) {
+         generator()
+   }
+
+
+// for (let i = 0; i < 10; i++) {
+//    generator()
+   
+// }
+
